8a0893cee06716dc7549e21d2f880ca6e0b64304
Anil Gupta <anilgupta@agupta.spokeo.lan>
2016-03-04 14:56:45 -0800

    Initial Respository with rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Addressbook::Application.config.secret_token = '26d34e85f2d76b0d6a84de4d11cb49427d92960e3297463463a2671ba28c409dc195d124e071c5e8fb6c129df600dc6bba44354cdc70ef64468c259d74e0ea9a'
